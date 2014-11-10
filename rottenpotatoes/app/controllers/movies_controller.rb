002dddc06055ad767fc4c473bd88e5122f88d35a
sivarama <sivarama@grinnell.edu>
2014-11-10 16:47:09 -0600

    started part 2

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -37,25 +37,32 @@ class MoviesController < ApplicationController
   end
 
   def new
-    # default: render 'new' template
+    @movie = Movie.new
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
+  @movie = Movie.new(params[:movie])
+  if @movie.save
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
+  else
+    render 'new' # note, 'new' template can access @movie's field values!
   end
+ end
 
   def edit
     @movie = Movie.find params[:id]
   end
 
   def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+  @movie = Movie.find params[:id]
+  if @movie.update_attributes(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
+  else
+    render 'edit' # note, 'edit' template can access @movie's field values!
   end
+ end
 
   def destroy
     @movie = Movie.find(params[:id])
